4704f4ee726cce6e015709606f25a7f6f5e3f37d
Paritosh Mishra <paritosh149@outlook.com>
2019-06-03 18:57:53 +1000

    prod build 001

---
+++ precache-manifest.423ab32795efebe4f34245ec22cbd988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f64c1d7ebe6d66d63dd8004e9073cb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeae3cea30bbcac21704",
+    "url": "/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "93cd55f45a7a88bb648d",
+    "url": "/static/js/2.e27cf4de.chunk.js"
+  },
+  {
+    "revision": "eeae3cea30bbcac21704",
+    "url": "/static/js/main.eff932e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
